c54eff7d249195fe19ae1085424f9d3b53ea8231
sekitaka <sekitaka.tech@gmail.com>
2022-06-10 12:56:45 +0900

    Initial commit

---
+++ docker/docker_helper.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+PROJECT_NAME="deployer_example"
+SCRIPT_DIR=$(cd $(dirname $0);pwd)
+
+usage(){
+  echo "Usage: docker_helper.sh command"
+}
+case $1 in
+  "up" )
+    docker-compose -p $PROJECT_NAME -f $SCRIPT_DIR/docker-compose.yml up -d
+  ;;
+  "stop" )
+    docker-compose -p $PROJECT_NAME -f $SCRIPT_DIR/docker-compose.yml stop
+  ;;
+  "build" )
+    docker-compose -p $PROJECT_NAME -f $SCRIPT_DIR/docker-compose.yml build
+  ;;
+  "attach_deployer" )
+    docker exec -it `docker ps -q -f name=${PROJECT_NAME}_deployer` bash
+  ;;
+  "attach_web" )
+    docker exec -it `docker ps -q -f name=${PROJECT_NAME}_web` bash
+  ;;
+  "attach_dev" )
+    docker exec -it `docker ps -q -f name=${PROJECT_NAME}_dev` bash
+  ;;
+  * )
+    usage
+  ;;
+esac
---
+++ documents/memo.md
@@ -0,0 +1,6 @@
+http://geekyplatypus.com/dockerise-your-php-application-with-nginx-and-php7-fpm/
+https://laravel.com/docs/9.x
+https://getcomposer.org/download/
+https://cloudraya.com/knowledge-base/how-to-setup-your-own-laravel-with-nginx-in-ubuntu-18-04/
+
+
